6188134aa67913157c5a18e295e52a840f529d03
sharath hanswadi <shanswadi@gmail.com>
2022-05-14 15:11:48 +0530

    fix: update commit changes

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "davoclock-aws-cicd-pipeline"
+        bucket = "sharath-new-bucket"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
